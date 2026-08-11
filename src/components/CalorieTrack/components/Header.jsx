c367c11c8d3f939d35a2f6d34ed550711aadb1ae
Billy Conard <billy.conard@pcc.edu>
2026-08-11 10:06:44 -0700

    fix: Removed the anchor tag and replaced with react Link element.

--- src/components/CalorieTrack/components/Header.jsx
+++ src/components/CalorieTrack/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router";
 
 // ============================================================
 // Header.jsx
@@ -36,10 +37,10 @@ const Header = () => {
             <header>
 
                 <div className="return-container">
-                    <a href="/" className="calculator-return-link"> Return to Dashboard</a>
+                    <Link to="/" className="calculator-return-link">Return to Dashboard</Link>
                 </div>
 
-                <div className="icon-container">
+                <div id="icon-container">
                     <svg fill="#F8F9FA" width="200px" height="200px" viewBox="0 0 15 15" id="fitness-centre" xmlns="http://www.w3.org/2000/svg">
                         <path id="daec40ff-71f5-4432-9d75-dcba7b9c1b89" d="M14.5,7V8h-1v2h-1v1H11V8H4v3H2.5V10h-1V8H.5V7h1V5h1V4H4V7h7V4h1.5V5h1V7Z"/>
                     </svg>
