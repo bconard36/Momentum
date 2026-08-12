2eacaf6288c42d3ed3aaf396d19515e39264319b
Billy Conard <billy.conard@pcc.edu>
2026-08-12 16:49:30 -0700

    docs: comments / notes about db operations and steps needed before connection can be made.

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,3 +1,7 @@
+/**
+ * Supabase Client Constructor 
+ 
+ */
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = import.meta.env.VITE_API_SUPABASE_URL;
@@ -6,3 +10,16 @@ const supabaseKey = import.meta.env.VITE_API_PUBLISHABLE_KEY;
 const supabase = createClient(supabaseURL, supabaseKey);
 
 export default supabase;
+
+// How should a user row be created?
+// Database Trigger — Postgres function that fires whenever a row is inserted into auth.users
+// Function inserts the matching row into users table server-side
+// first_name and last_name should be in the db trigger 
+
+// auth.users enforces unique email
+// Unique constraint kept for email in users table for data integrity
+
+// With email being stored in public and auth, an INSERT trigger is needed on auth.users
+// If UPDATE email is a feature, an UPDATE trigger is also needed! 
+
+// 8/12/26 - Build UI for login and create account first. Ensure the proper data is captured. Then begin DB operations. 
