fc3027ffa61ed69ff1aff4ac538c638edb5bf2fd
Billy Conard <billy.conard@pcc.edu>
2026-08-13 15:03:51 -0700

    feat: Added a protected route component. This acts as a wrapper/guard to the Dashboard component. getSession is invoked, and the session value is converted to a boolean to confirm or deny authentication.

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+import { Navigate } from "react-router";
+
+/**
+ * Protected Route Component 
+ * Checks the current Supabase authentication session before rendering protected content. 
+ * Displays a loading state while the session is being checked, redirects unauthenticated users to the sign-in route, 
+ * and renders the provided child component for authenticated users.
+ * @param {React.ReactNode} children - child components(s) whose routes are to be protected  
+ * @returns {Object} route to navigate back to 
+ * @returns {JSX.Element} Loading state, redirect, or protected child component
+ */
+const ProtectedRoute = ({ children }) => {
+
+    // Track state for loading and authentication 
+    const [loading, setLoading] = useState(true);
+    const [authenticated, setAuthenticated] = useState(false);
+
+    useEffect(() => {
+        supabase.auth.getSession() // Retrieves user session data
+            .then(({ data: { session } }) => {
+                setAuthenticated(!!session); // Convert session value to a boolean: session exists = authenticated 
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div className="loading-message">Loading...</div>
+    }
+
+    if (!authenticated) {
+        return <Navigate to="/" replace />
+    }
+
+    return children;
+}
+
+export default ProtectedRoute;
