3be41e5000ed569afe4029eb4b87c6a48e36e508
Billy Conard <billy.conard@pcc.edu>
2026-08-13 08:49:07 -0700

    fix: Replaced old export default like with an export const instead. Supabase client was not rendering with export default

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -7,9 +7,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = import.meta.env.VITE_API_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_API_PUBLISHABLE_KEY;
 
-const supabase = createClient(supabaseURL, supabaseKey);
-
-export default supabase;
+export const supabase = createClient(supabaseURL, supabaseKey);
 
 // How should a user row be created?
 // Database Trigger — Postgres function that fires whenever a row is inserted into auth.users
