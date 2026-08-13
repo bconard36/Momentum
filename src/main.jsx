279aa2fd0021f4bbaddc56803513238ad46e156a
Billy Conard <billy.conard@pcc.edu>
2026-08-13 15:13:53 -0700

    feat: added a new CSS file for sign in and sign up components, imported the CSS file in main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/base.css'
+import './styles/signInSignUp.css'
 import './styles/calculator.css'
 import './styles/workoutForm.css'
 import './styles/workoutLog.css'
