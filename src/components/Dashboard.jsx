88fe9bd2c48047155eb682363c3ffe554b0ca18a
Billy Conard <billy.conard@pcc.edu>
2026-08-13 12:59:05 -0700

    feat: added a user icon to the dashboard component and incorporated sign out functionality. Successful logout redirects to sign-in component.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import WorkoutLog from "./WorkoutLog";
+import { supabase } from "../utils/supabaseClient";
 
 // ============================================================
 // Dashboard.jsx
@@ -22,9 +23,36 @@ import WorkoutLog from "./WorkoutLog";
 // ============================================================
 
 const Dashboard = ({ savedWorkouts, deleteWorkout }) => {
+
+    const logOutNav = useNavigate();
+
+    const signOut = async () => {
+        try {
+            const { error } = await supabase.auth.signOut();
+
+            if (error) {
+                console.log("Error signing out:", error);
+            } else {
+                console.log("Signed out!");
+                logOutNav("/sign-in", { replace: true });
+            }
+        } catch (error) {
+            console.error("Error signing out:", error);
+        }
+    }
+
     return (
         <>
             <div className="dashboard">
+                <div className="user-icon-container">
+                    <svg width="35px" height="35px" viewBox="0 0 16 16" style={{
+                            fill: "currentColor"
+                        }} xmlns="http://www.w3.org/2000/svg" onClick={signOut}>
+                        <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 
+                            5 4C5 5.65685 6.34315 7 8 7Z" />
+                        <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" />
+                    </svg>
+                </div>
                 <header className="dashboard-header">
                     <h1 className="dashboard-title">Momentum</h1>
                     <p className="dashboard-subtitle">
