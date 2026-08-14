079b0f67c09bf8b59924463fddb110d6f6d27920
Billy Conard <billy.conard@pcc.edu>
2026-08-14 08:45:46 -0700

    fix: replaced the removed Link import, removed the dimension attributes from the svg

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import WorkoutLog from "./WorkoutLog";
 import { supabase } from "../utils/supabaseClient";
 import { useState } from "react";
@@ -62,9 +62,11 @@ const Dashboard = ({ savedWorkouts, deleteWorkout }) => {
         <>
             <div className="dashboard">
                 <div className="user-icon-container">
-                    <svg width="35px" height="35px" viewBox="0 0 16 16" style={{
+                    <svg viewBox="0 0 16 16" 
+                        style={{
                             fill: "currentColor"
-                        }} xmlns="http://www.w3.org/2000/svg" onClick={() => toggleUserMenu()}>
+                        }} 
+                        xmlns="http://www.w3.org/2000/svg" onClick={() => toggleUserMenu()}>
                         <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 
                             5 4C5 5.65685 6.34315 7 8 7Z" />
                         <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" />
