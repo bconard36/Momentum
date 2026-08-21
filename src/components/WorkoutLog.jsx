437286a59a0322311167cbec5878510098e5ed8f
Billy Conard <billy.conard@pcc.edu>
2026-08-21 11:10:03 -0700

    refactor: capitalized the first letter of each word in the workout log

--- src/components/WorkoutLog.jsx
+++ src/components/WorkoutLog.jsx
@@ -168,7 +168,7 @@ const WorkoutLog = ({ savedWorkouts, deleteWorkout, fetchWorkoutLog }) => {
         // Otherwise, return the descending (newer dates first)
         return sortOrder === 'asc' ? timeA - timeB : timeB - timeA;
     });
-
+    
      /**
      * Toggles sort order between ascending and descending, and flips the
      * sort icon's rotation state to match.
