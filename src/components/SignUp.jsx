9376c58d70c6f0af3766b8e2b9dc96f0961b7fd2
Billy Conard <billy.conard@pcc.edu>
2026-08-21 13:35:49 -0700

    fix: added email pattern checks for sign up component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -127,7 +127,11 @@ const SignUp = () => {
                             name="email"
                             id="email"
                             {...register("email", {
-                                required: "Email is required."
+                                required: "Email is required.",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: "Invalid email address"
+                                }
                             })}
                         />
                     </div>
