8da7c730de3443707ab73faf2e2bf29e1cb8df57
Billy Conard <billy.conard@pcc.edu>
2026-08-21 07:26:40 -0700

    docs: updated comments

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,8 +4,8 @@ import { Navigate } from "react-router";
 
 /**
  * Protected Route Component 
- * Checks the current Supabase authentication session before rendering protected content. 
- * Displays a loading state while the session is being checked, redirects unauthenticated users to the sign-in route, 
+ * Sends an HTTP request to Supabase auth server to verify User's authenticated JWT 
+ * Displays a loading state while user is being checked, redirects unauthenticated users to the sign-in route, 
  * and renders the provided child component for authenticated users.
  * @param {React.ReactNode} children - child components(s) whose routes are to be protected  
  * @returns {Object} route to navigate back to 
@@ -18,9 +18,9 @@ const ProtectedRoute = ({ children }) => {
     const [authenticated, setAuthenticated] = useState(false);
 
     useEffect(() => {
-        supabase.auth.getUser() // Retrieves user session data
+        supabase.auth.getUser() // Retrieves authenticated user data
             .then(({ data: { user } }) => {
-                setAuthenticated(!!user); // Convert session value to a boolean: session exists = authenticated 
+                setAuthenticated(!!user); // Convert user object to a boolean: user exists = authenticated 
                 setLoading(false);
             });
     }, []);
