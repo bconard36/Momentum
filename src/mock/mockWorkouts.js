6ed9e7a3a3ae70894a70f51f971452d9475d96e7
Billy Conard <billy.conard@pcc.edu>
2026-08-31 13:26:35 -0700

    feat: added a mockWorkouts file to be used in the landing page

---
+++ src/mock/mockWorkouts.js
@@ -0,0 +1,40 @@
+// src/mock/mockWorkouts.js
+
+export const mockWorkouts = [
+  {
+    date: "2026-08-24",
+    exercises: [
+      { name: "Back Squat", type: "strength", sets: 3, reps: 10, weight: 210 },
+      {
+        name: "Trail Running",
+        type: "duration",
+        duration_minutes: 45,
+        duration_seconds: 22,
+      },
+    ],
+  },
+  {
+    date: "2026-08-22",
+    exercises: [
+      { name: "Bench Press", type: "strength", sets: 4, reps: 8, weight: 165 },
+    ],
+  },
+  {
+    date: "2026-08-20",
+    exercises: [
+      { name: "Deadlift", type: "strength", sets: 3, reps: 5, weight: 275 },
+      {
+        name: "Rowing Machine",
+        type: "duration",
+        duration_minutes: 20,
+        duration_seconds: 0,
+      },
+      {
+        name: "Plank",
+        type: "duration",
+        duration_minutes: 3,
+        duration_seconds: 15,
+      },
+    ],
+  },
+];
