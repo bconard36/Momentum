6b2ce614c9d7c3b93f46dbd4cc4234816e81c806
Billy Conard <billy.conard@pcc.edu>
2026-09-01 08:30:57 -0700

    docs: updated/clarified JSDoc comments

--- src/hooks/useAuthUser.js
+++ src/hooks/useAuthUser.js
@@ -1,13 +1,23 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
 
+/**
+ * Hook that retrieves the current authenticated user and listens for auth state changes
+ * Sets up a real-time listener that updates when the user logs in/out
+ *
+ * @returns {Object | undefined} Current user object or undefined
+ */
+
 export function useAuthUser() {
   const [user, setUser] = useState(undefined);
 
   useEffect(() => {
+    // Set initial user
     supabase.auth.getUser().then(({ data: { user } }) => setUser(user));
 
+    // Set real-time listener to watch for auth changes
     const { data: listener } = supabase.auth.onAuthStateChange(() => {
+      // Runs whenever auth state changes
       supabase.auth.getUser().then(({ data: { user } }) => setUser(user));
     });
 
