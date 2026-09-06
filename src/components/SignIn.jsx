f81f452ddccd5fe579d1d8517a527abcb5a39374
Billy Conard <billy.conard@pcc.edu>
2026-09-06 16:51:09 -0700

    fix: changed the font on the sign in screen

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -73,7 +73,7 @@ const SignIn = () => {
           {/* Render the sign in form when no login errors detected */}
           {!errors.root?.message && (
             <>
-              <h1 className="dashboard-title" id="form-title">
+              <h1 className="sign-in-title" id="form-title">
                 Momentum
               </h1>
               <div className="sign-in-form-group">
