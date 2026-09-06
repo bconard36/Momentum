0db8904e55167f29f9a63aecc4f03bdac9b5109c
Billy Conard <billy.conard@pcc.edu>
2026-09-06 13:15:36 -0700

    feat: added an email input component

---
+++ src/components/EmailInput.jsx
@@ -0,0 +1,24 @@
+const EmailInput = ({ register, type, name, id, errors }) => {
+  return (
+    <>
+      <label htmlFor={id}>Email Address</label>
+      {errors.email && (
+        <span className="error-message">{errors.email.message}</span>
+      )}
+      <input
+        type={type}
+        name={name}
+        id={id}
+        {...register(name, {
+          required: "Email address is required.",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Invalid email address",
+          },
+        })}
+      />
+    </>
+  );
+};
+
+export default EmailInput;
