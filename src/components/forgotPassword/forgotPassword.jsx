3fdd87cfe0e49bdd75bedec52820057cf8ab6710
Billy Conard <billy.conard@pcc.edu>
2026-09-18 11:27:08 -0700

    refactor: modified the path for return link on forgot password component

--- src/components/forgotPassword/forgotPassword.jsx
+++ src/components/forgotPassword/forgotPassword.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { Link } from "react-router";
 import EmailInput from "../EmailInput";
 import { supabase } from "../../utils/supabaseClient";
 
@@ -22,23 +23,33 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="forgot-password-form-container">
-      <h2>Forgot Password Email Verification</h2>
-      <form className="forgot-password-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="password-reset-form-group">
-          <EmailInput
-            register={register}
-            type="email"
-            name="forgot_password_email"
-            id="forgot_password_email"
-            errors={errors}
-          />
-        </div>
-        <button className="secondary-button forgot-password-button">
-          Reset Forgotten Password
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="return-container">
+        <Link to="/sign-in" className="return-link" id="forgot-password-return">
+          Return to Sign In
+        </Link>
+      </div>
+      <div className="forgot-password-form-container">
+        <h2>Forgot Password Email Verification</h2>
+        <form
+          className="forgot-password-form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="password-reset-form-group">
+            <EmailInput
+              register={register}
+              type="email"
+              name="forgot_password_email"
+              id="forgot_password_email"
+              errors={errors}
+            />
+          </div>
+          <button className="secondary-button forgot-password-button">
+            Reset Forgotten Password
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
