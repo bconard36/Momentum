899915328af2460ebc385a65fce0ccf7592b04bf
Billy Conard <billy.conard@pcc.edu>
2026-09-21 14:39:07 -0700

    feat: added logic for updating password but is commented out for now. Displays success and error messages, no DB logic yet, just UI

--- src/components/forgotPassword/updateForgottenPassword.jsx
+++ src/components/forgotPassword/updateForgottenPassword.jsx
@@ -1,7 +1,13 @@
 import { useForm } from "react-hook-form";
 import PasswordInput from "../PasswordInput";
+import { useState } from "react";
+import { supabase } from "../../utils/supabaseClient";
+import { Link } from "react-router";
 // Will need to accept reset token for authentication!
 const UpdateForgottenPassword = () => {
+  const [formSuccess, setFormSuccess] = useState(false);
+  const [formError, setFormError] = useState(null); // Same format as account setting form - error name and message properties
+
   const {
     register,
     handleSubmit,
@@ -14,9 +20,26 @@ const UpdateForgottenPassword = () => {
     },
   });
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     try {
-      console.log(`Password to be updated: ${data.update_forgotten_password}`);
+      console.log(
+        `Call supabase.auth.updateUser() with password to set as new: ${data.update_forgotten_password}`,
+      );
+      // Attempt to update password
+      //   const { data: passwordReset, error: passwordResetError } =
+      //     await supabase.auth.updateUser({
+      //       password: passwordReset,
+      //     });
+
+      //   if (passwordResetError) {
+      //     console.log(`Error updating password: ${passwordResetError.message}`);
+      //   } else {
+      //     console.log(
+      //       `Success — password updated. New password: ${passwordReset}`,
+      //     );
+      //   }
+      setFormSuccess(true);
+      setFormError(null);
     } catch (error) {
       console.error(`Error updating password: ${error}`);
     }
@@ -24,34 +47,61 @@ const UpdateForgottenPassword = () => {
 
   return (
     <div className="update-forgotten-password-container">
-      <h2>Update Your Password</h2>
-      <form
-        className="update-forgotten-password-form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="update-forgotten-password-field">
-          <PasswordInput
-            register={register}
-            label="update_forgotten_password"
-            name="update_forgotten_password"
-            id="update_forgotten_password"
-            errors={errors}
-          />
+      {formSuccess && (
+        <div className="forgotten-password-reset-success">
+          <h2>Success!</h2>
+          <h4>Your password has been reset.</h4>
+          <p>Redirecting to sign in...</p>
+          <p className="manual-redirect-container">
+            If page does not automatically refresh after 5 seconds,{" "}
+            <Link to="/sign-in" className="manual-redirect">
+              click here
+            </Link>
+            .
+          </p>
         </div>
-        <div className="update-forgotten-password-field">
-          <PasswordInput
-            register={register}
-            label="confirm_forgotten_password_update"
-            name="confirm_forgotten_password_update"
-            id="confirm_forgotten_password_update"
-            errors={errors}
-            matchValue={watch("update_forgotten_password")}
-          />
+      )}
+      {!formSuccess && formError !== null && (
+        <div className="forgotten-password-reset-failure">
+          <span className="error-message">Error updating password.</span>
+          <p>{formError}</p>
+          <Link to="/forgot-password" className="password-reset-error-redirect">
+            Please try again
+          </Link>
         </div>
-        <button className="primary-button update-forgotten-password-button">
-          Update Password
-        </button>
-      </form>
+      )}
+      {!formSuccess && formError === null && (
+        <>
+          <h2>Update Your Password</h2>
+          <form
+            className="update-forgotten-password-form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="update-forgotten-password-field">
+              <PasswordInput
+                register={register}
+                label="update_forgotten_password"
+                name="update_forgotten_password"
+                id="update_forgotten_password"
+                errors={errors}
+              />
+            </div>
+            <div className="update-forgotten-password-field">
+              <PasswordInput
+                register={register}
+                label="confirm_forgotten_password_update"
+                name="confirm_forgotten_password_update"
+                id="confirm_forgotten_password_update"
+                errors={errors}
+                matchValue={watch("update_forgotten_password")}
+              />
+            </div>
+            <button className="primary-button update-forgotten-password-button">
+              Update Password
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
